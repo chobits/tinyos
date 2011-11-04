c803bc58d97549ac7314b9de2e8eee7434d87f8c
Xiaochen Wang <wangxiaochen0@gmail.com>
2011-11-04 19:08:32 +0800

    Find a bug when using test script(TEST_EXIT) in user/init.c
      The bug is that:
      Init forks the maximum number of childs, leting all childs execute("/hello", ...),
       then all childs("/hello") exit.
      When all these run again, page fault occurs when child(pid 67) runs!
      From single-step debugging, I found that child(pid 67) return from syscall(SYS_fork)
       to error code address 0xbfffxxxx.

--- user/init.c
+++ user/init.c
@@ -1,53 +1,65 @@
 #include <ulib.h>
 
+#define iprintf(fmt, args...) printf("/init: " fmt, ##args)
+
 int main(int argc, char **argv)
 {
-	printf("-----------[In user mode]--------------\n");
+	iprintf("we are now in user mode\n");
+
 #ifdef TEST_FORK
 	int pid;
 	while (1) {
 		pid = fork();
 		if (pid < 0) {
-			printf("Fork error\n");
+			iprintf("Fork error\n");
 			break;
 		} else if (pid == 0) {
-			printf("I'm child %d\n", getpid());
+			iprintf("I'm child %d\n", getpid());
 			break;
 		} else {
-			printf("Create child %d\n", pid);
+			iprintf("Create child %d\n", pid);
 		}
 	}
 	while (1)
 		yield();
 #endif
 
+#define TEST_EXIT
 #ifdef TEST_EXIT
 	int pid;
 	int rv;
 refork:
 	pid = fork();
 	if (pid < 0) {
-		printf("[%d] Fork error\n", getpid());
+		iprintf("[%d] Fork error\n", getpid());
 	} else if (pid == 0) {
 refork_child:
 		pid = fork();
 		if (pid < 0) {
-			printf("[%d] Fork error\n", getpid());
-			printf("[%d] exit\n", getpid());
+			iprintf("[%d] Fork error\n", getpid());
+			iprintf("[%d] exit\n", getpid());
 			exit(getpid());
 		} else if (pid == 0) {
-			printf("[%d] exit\n", getpid());
+			char *as[] = { "/hello", "will exit\n", NULL };
+			/*
+			 * This bug is so strange:
+			 *  Testing this script, you will get a panic!
+			 * But add "iprintf("EXEC %d\n", pid);" here,
+			 * this bug will disappear!
+			 */
+			execute("/hello", 2, as);
+			while (1) ;
 			exit(getpid());
 		} else {
-			printf("[%d] create %d\n", getpid(), pid);
+			iprintf("[%d] create %d\n", getpid(), pid);
 		}
 		goto refork_child;
 	} else {
-		printf("[%d] Create child %d and let it run!\n", getpid(), pid);
+		iprintf("[%d] Create child %d and let it run!\n", getpid(), pid);
 		yield();
-		printf("[%d] waking up!\n", getpid());
+		iprintf("[%d] waking up!\n", getpid());
 		while ((pid = wait(&rv)) > 0)
-			printf("[p] Child %d exit %d\n", pid, rv);
+			iprintf("[p] Child %d exit %d\n", pid, rv);
 		goto refork;
 	}
 	while (1) ;
@@ -60,19 +72,19 @@ refork_child:
 refork:
 	pid = fork();
 	if (pid < 0) {
-		printf("fork error\n");
+		iprintf("fork error\n");
 	} else if (pid == 0) {
 		r = execute("/hello", 3, argvs);
-		printf("error for execute (%d)\n", r);
+		iprintf("error for execute (%d)\n", r);
 		while (1) ;
 	}
-	printf("[%d] Create task %d\n", getpid(), pid);
+	iprintf("[%d] Create task %d\n", getpid(), pid);
 	yield();
 	pid = wait(&r);
 	if (pid > 0)
-		printf("[%d] wait child %d return %x\n", getpid(), pid, r);
+		iprintf("[%d] wait child %d return %x\n", getpid(), pid, r);
 	else
-		printf("[%d] wait error\n");
+		iprintf("[%d] wait error\n");
 	goto refork;
 #endif
 
@@ -80,9 +92,9 @@ refork:
 	char *argvs[] = { "hello", "world", "Haaaa!", NULL };
 	int i;
 	for (i = 0; i < argc; i++)
-		printf("[%d] %s\n", i, argv[i]);
+		iprintf("[%d] %s\n", i, argv[i]);
 	execute("init", 3, argvs);
-	printf("execute error\n");
+	iprintf("execute error\n");
 	while (1) ;
 #endif
 
@@ -92,43 +104,42 @@ refork:
 	int size, i;
 	fd = open("/text", 0);
 	while ((size = read(fd, buf, 4)) > 0)
-		printf("%*s", size, buf);
+		iprintf("%*s", size, buf);
 	if (size < 0) {
-		printf("read return %d\n", size);
+		iprintf("read return %d\n", size);
 		goto err;
 	}
-	printf("read ok!\n");
+	iprintf("read ok!\n");
 	if (lseek(fd, 0, SEEK_SET) != 0) {
-		printf("lseek error\n");
+		iprintf("lseek error\n");
 		goto err;
 	}
 	/* (7 + 512 + 1)*1024 */
 	i = 832480;
 	while (i > 0) {
 		if ((size = write(fd, "123456789\n", 10)) != 10) {
-			printf("write error %d\n", size);
+			iprintf("write error %d\n", size);
 			goto err;
 		}
 		i -= 10;
 	}
-	printf("write ok!\n");
+	iprintf("write ok!\n");
 	fsync(fd);
-	printf("fsync ok!\n");
+	iprintf("fsync ok!\n");
 	fd = close(fd);
 err:
 	while (1) ;
 #endif
 
-#define TEST_GETS
 #ifdef TEST_GETS
 	char buf[64];
 	int size;
 	while ((size = gets(buf, 64)) > 0)
-		printf("[%d]%*s", size, size, buf);
-	printf("ERROR gets return %d\n", size);
+		iprintf("[%d]%*s", size, size, buf);
+	iprintf("ERROR gets return %d\n", size);
 	while (1) ;
 #endif
-	printf("I'm process %d\n", getpid());
+	iprintf("I'm process %d\n", getpid());
 	while (1) ;
 	return 0;
 }
